f8b575e68a13bc9460ec282e19efcab6ce8443fd
rsuttles58 <robert.suttles58@gmail.com>
2019-05-26 12:07:16 -0500

    Added Note model schema and renamed the folder to align with index file.

--- server.js
+++ server.js
@@ -5,39 +5,49 @@ const mongoose = require("mongoose");
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-var db = require ("./models");
+var db = require("./models");
 
 var PORT = 8080;
 
 var app = express();
 
 app.use(logger("dev"));
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/wsjArticles", {useNewUrlParser:true});
 
-app.get("/scrape", function(req,res){
-    axios.get("https://www.wsj.com/news/business/industrial-services").then(function(response){
+app.get("/scrape", function (req, res) {
+    axios.get("https://www.wsj.com/news/business/industrial-services").then(function (response) {
         var $ = cheerio.load(response.data);
-        $("article").each(function(i, element){
+        $("article.WSJTheme__story_pKzwqDTtmhBsWmw3xOcLr").each(function (i, element) {
             var result = {};
 
-            result.headline = $(this)
-            .children(".WSJTheme_headline_19_2KfxGdC8OTxXXrZcwJ2 a")
-            .text();
-            result.url = $(this)
-            .children(".WSJTheme_headline_19_2KfxGdC8OTxXXrZcwJ2 a")
-            .attr("href");
-            result.summary = $(this)
-            .children("p")
-            .text();
-            result.artCategory= $(this)
-            .children(".WSJTheme_articleType_WZo32uUhVLQVoD6_eTR7q span")
-            .text();
+            // result.headline = $(this)
+            // .children(".WSJTheme_headline_19_2KfxGdC8OTxXXrZcwJ2 a")
+            // .text();
 
-            console.log(result);
+            result.headline = $(this)
+                .find("div.WSJTheme__articleType_WZo32uUhVLQVoD6_eTR7q").find("h3.WSJTheme__headline_19_2KfxGdC8OTxXXrZcwJ")
+            .find("a").text();
+            result.url = $(this)
+                .children(".WSJTheme_headline_19_2KfxGdC8OTxXXrZcwJ2 a")
+                .attr("href");
+            result.summary = $(this)
+                .children("p")
+                .text();
+            result.artCategory = $(this)
+                .children(".WSJTheme_articleType_WZo32uUhVLQVoD6_eTR7q span")
+                .text();
+
+            console.log(result.headline);
+
         })
     })
+    res.send("Scrape Complete");
 })
+
+app.listen(PORT, function () {
+    console.log("App running on port " + PORT + "!");
+});
