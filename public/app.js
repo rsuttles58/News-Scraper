c471740b57655c817658423fc7c10f6104144f9e
rsuttles58 <robert.suttles58@gmail.com>
2019-05-26 21:16:21 -0500

    Rendered data is now styled, formatted, and organized.  Includes all scraped fields.

--- public/app.js
+++ public/app.js
@@ -2,8 +2,9 @@ $.getJSON("/articles", function(data) {
 
     for (var i = 0; i < data.length; i++) {
 
-      $("#articleHolder").append("<p data-id='" + data[i]._id + "'>" + data[i].title 
-      + "<br />" + "Link: " + data[i].url + "<br />" + data[i].summary + "</p>"
-      + "------------------------------------");
+      $("#articleHolder").append("<p data-id='" + data[i]._id + "'>" + "<b>" +data[i].title + "</b>" 
+      + "<br />" + "</p>" + "<p>" + data[i].summary + "<br />" + "</p>" + "<p>" + "Link: " + data[i].url + "</p>"
+      + "<p>" + "Posted: " + data[i].timeStamp  + " ago" +"</p>"
+      + "--------------------------------------------------------------------");
     }
   });
